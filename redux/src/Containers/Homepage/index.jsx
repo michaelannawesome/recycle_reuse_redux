f02eefae163a580cc25b5b9e582a43859356326f
michaelannawesome <michaelannawesome@gmail.com>
2021-05-31 19:04:38 -0700

    fix errors and api call

--- redux/src/Containers/Homepage/index.jsx
+++ redux/src/Containers/Homepage/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
 import { makeSelectUsers } from "./selectors";
+import Axios from "axios";
 
 const stateSelector = createSelector(makeSelectUsers, (users) => ({
   users,
@@ -9,6 +10,13 @@ const stateSelector = createSelector(makeSelectUsers, (users) => ({
 
 export function HomePage(props) {
   const { users } = useSelector(stateSelector);
-  console.log("Users: ", users);
+
+  const fetchUsers = async () => {
+    const response = await Axios.get("https://reqres.in/api/users").catch(
+      (err) => {
+        console.log("Err: ", err);
+      }
+    );
+  };
   return <div>Hello React</div>;
 }
